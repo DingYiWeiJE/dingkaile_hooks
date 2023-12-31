8ce9a1156a444fdf48d242f200ffa60366666605
w_dingzhipeng <w_dingzhipeng@kingsoft.com>
2023-12-31 17:21:35 +0800

    [task] 自动化部署

--- config/config.ts
+++ config/config.ts
@@ -6,7 +6,7 @@ export default {
     type: 'none',
     exclude: [],
   },
-  publicPath: '/encode-hooks/',
+  publicPath: '/dingkaile_hooks/',
   history: { type: 'hash' },
   extraBabelPlugins: [
     [
@@ -20,14 +20,14 @@ export default {
   ],
   title: '印客学院 encode hooks',
   mode: 'site',
-  favicon: '/encode-hooks/avatar.png',
-  logo: '/encode-hooks/avatar.png',
+  favicon: '/dingkaile_hooks/avatar.png',
+  logo: '/dingkaile_hooks/avatar.png',
   dynamicImport: {},
   manifest: {},
   hash: true,
   alias: {
     encodeHooks: process.cwd() + '/packages/hooks/src/index.ts',
-    ['encode-hooks']: process.cwd() + '/packages/hooks/src/index.ts',
+    ['dingkaile_hooks']: process.cwd() + '/packages/hooks/src/index.ts',
   },
   resolve: {
     includes: ['docs', 'packages/hooks/src'],
@@ -42,7 +42,7 @@ export default {
   navs: [
     { title: '指南', path: '/guide' },
     { title: 'Hooks', path: '/hooks' },
-    { title: 'GitHub', path: 'https://github.com/encode-studio-fe/encode-hooks' },
+    { title: 'GitHub', path: 'https://github.com/encode-studio-fe/dingkaile_hooks' },
   ],
   menus: {
     '/': [
